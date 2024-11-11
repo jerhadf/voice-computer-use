3f19f7117a924534ea42823c13f7740c62176c51
twitchard <richard.marmorstein@gmail.com>
2024-11-11 14:02:04 -0800

    wip

--- computer_use_demo/tools/bash.py
+++ computer_use_demo/tools/bash.py
@@ -12,7 +12,7 @@ class _BashSession:
 
     command: str = "/bin/bash"
     _output_delay: float = 0.2  # seconds
-    _timeout: float = 120.0  # seconds
+    _timeout: float = 5.0  # seconds
     _sentinel: str = "<<exit>>"
 
     def __init__(self):
